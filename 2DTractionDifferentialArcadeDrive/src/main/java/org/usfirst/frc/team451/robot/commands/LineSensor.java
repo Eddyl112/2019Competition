3959b70847ecc1f12466c800e804b80681625fff
Syberkonda <cyberconcoder@gmail.com>
2019-01-26 15:11:42 -0500

    Successful Array Test w/ Line Sensors
    Line Sensors have been proven to work in an array. This is being pushed because we can't pull Alex's uploads (mostly)

--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/LineSensor.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/LineSensor.java
@@ -28,11 +28,14 @@ public class LineSensor extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    /*Outputs the current status of the line sensors on SmartDashboard. Green = white detected,
+    /* Outputs the current status of the line sensors on SmartDashboard. Green = white detected,
     brown = white not detected */
-    for(int i = 0; i<Sensor.Sensors.length;i++){
-      SmartDashboard.putBoolean(Sensor.SensorLabels[i]+" Line Sensor", Sensor.Sensors[i].get());
+    for(int i = 0; i<Sensor.Sensors.length; i++) {
+      SmartDashboard.putBoolean(Sensor.SensorLabels[i]+ " Line Sensor", Sensor.Sensors[i].get());
     }
+
+    
+
     }
 
   // Make this return true when this Command no longer needs to run execute()
