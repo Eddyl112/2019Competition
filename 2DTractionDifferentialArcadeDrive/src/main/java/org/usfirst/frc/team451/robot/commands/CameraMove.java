c2b668bc038b38303f284f8a21836b04386c73f9
Eddyl112 <edwardlui17@gmail.com>
2019-02-03 16:50:52 -0500

    cleaning

--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/CameraMove.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/CameraMove.java
@@ -11,6 +11,7 @@ import org.usfirst.frc.team451.robot.Robot;
 import org.usfirst.frc.team451.robot.subsystems.CameraServo;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CameraMove extends Command {
   public CameraMove() {
@@ -29,24 +30,27 @@ public class CameraMove extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if (OI.driveStickLeft.getPOV() == 0) {
-      CameraServo.pitchSpeed = CameraServo.pitchSpeed + 0.1;  
-      CameraServo.cameraPitch.set(CameraServo.pitchSpeed);
-
-      System.out.println(CameraServo.pitchSpeed);
-    } else if(OI.driveStickLeft.getPOV() == 180){
-      CameraServo.pitchSpeed = CameraServo.pitchSpeed - 0.1;
-      CameraServo.cameraPitch.set(CameraServo.pitchSpeed);
-      System.out.println(CameraServo.pitchSpeed);
-    } else if(OI.driveStickLeft.getPOV() == 90 ) {
-      CameraServo.yawSpeed = CameraServo.yawSpeed + 0.1;
+    if(CameraServo.pitchSpeed < 1 && CameraServo.pitchSpeed > -1) {
+      if (OI.driveStickLeft.getPOV() == 0) {
+        CameraServo.pitchSpeed = CameraServo.pitchSpeed + 0.01;  
+        CameraServo.cameraPitch.set(CameraServo.pitchSpeed);
+      } else if(OI.driveStickLeft.getPOV() == 180){
+        CameraServo.pitchSpeed = CameraServo.pitchSpeed - 0.01;
+        CameraServo.cameraPitch.set(CameraServo.pitchSpeed);
+      } 
+    } else {
+      SmartDashboard.putBoolean("Camera Movement Overload", true);
+    }
+  
+    
+    
+    if(OI.driveStickLeft.getPOV() == 90 ) {
+      CameraServo.yawSpeed = CameraServo.yawSpeed + 0.01;
       CameraServo.cameraYaw.set(CameraServo.yawSpeed);
-      System.out.println(CameraServo.yawSpeed);
     } else if(OI.driveStickLeft.getPOV() == 270) {
-      CameraServo.yawSpeed = CameraServo.yawSpeed - 0.1;
+      CameraServo.yawSpeed = CameraServo.yawSpeed - 0.01;
       CameraServo.cameraYaw.set(CameraServo.yawSpeed);
-      System.out.println(CameraServo.yawSpeed);
-    } //Change around, 0 is full left and 1 is full right
+    } 
   }
 
   // Make this return true when this Command no longer needs to run execute()
