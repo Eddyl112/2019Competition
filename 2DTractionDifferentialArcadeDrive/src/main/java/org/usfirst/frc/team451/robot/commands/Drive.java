aee5be9b9425c51f759676494862c2a774b6d5d7
Syberkonda <cyberconcoder@gmail.com>
2019-02-11 19:54:10 -0500

    I hope this works
    Should have elevator dpad code in it

--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/Drive.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/Drive.java
@@ -6,14 +6,12 @@ package org.usfirst.frc.team451.robot.commands;
 import org.usfirst.frc.team451.robot.Robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-//import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.DemandType;
 
 import org.usfirst.frc.team451.robot.OI;
 import org.usfirst.frc.team451.robot.subsystems.DriveTrain;
-import org.usfirst.frc.team451.robot.subsystems.LineTracker;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drive extends Command {
     public Double deadzone = 0.25; //Could be issue, would test
@@ -32,64 +30,43 @@ public class Drive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
-         //Register user imput from left drive stick   
-        if (OI.driveStickLeft.getY() < deadzone && OI.driveStickLeft.getY() > -deadzone) {
-            DriveTrain.wheelSpeed[0] = 0;
-        } else {
-            DriveTrain.wheelSpeed[0] = -OI.driveStickLeft.getY();
-        }
-
-        //register input from right drive stick
-        if (OI.driveStickRight.getY() < deadzone && OI.driveStickRight.getY() > -deadzone) {
-            DriveTrain.wheelSpeed[1] = 0;
-        } else {
-            DriveTrain.wheelSpeed[1] = OI.driveStickRight.getY();
-        }
-
-        //register user input from joystick trigger to enable user assist
-        if(OI.driveStickRight.getRawButton(1)) {
-            DriveTrain.userAssistEnabled = true;
-            //set the left side motion equal to the right side when driving straight forward
-            DriveTrain.wheelSpeed[0] = - DriveTrain.wheelSpeed[1];
-        } else DriveTrain.userAssistEnabled = false;
-
-        SmartDashboard.putBoolean("User Assist",DriveTrain.userAssistEnabled);
-
-        //Modify user input to keep the bot on the line during user assist
-        if(DriveTrain.userAssistEnabled){
-            if(!LineTracker.Sensors[0].get()) {
-                //If the left line sensor is tripped, then increase left wheel speed
-                DriveTrain.wheelSpeed[0] += Robot.UserAssistCorrectionSpeed/100;
-                System.out.println("Correcting right: "+DriveTrain.wheelSpeed[0]);
+        if(!Robot.LineTracker.AUTO){
+            if (OI.driveStickLeft.getY() < deadzone && OI.driveStickLeft.getY() > -deadzone) {
+                DriveTrain.wheelSpeed[0] = 0;
+            } else {
+                DriveTrain.wheelSpeed[0] = -OI.driveStickLeft.getY();
             }
-            if(!LineTracker.Sensors[2].get()){
-                //If the right line sensor is tripped, then increase the right wheel speed
-                DriveTrain.wheelSpeed[1] -= Robot.UserAssistCorrectionSpeed/100;
-                System.out.println("Correcting left: "+DriveTrain.wheelSpeed[1]);
 
+            if (OI.driveStickRight.getY() < deadzone && OI.driveStickRight.getY() > -deadzone) {
+                DriveTrain.wheelSpeed[1] = 0;
+            } else {
+                DriveTrain.wheelSpeed[1] = OI.driveStickRight.getY();
             }
-        }
-        
-        //Normally, run left motor based on left motor input (equal to modified right side durin user assist)
-        DriveTrain.frontLeftMotor.set(ControlMode.PercentOutput, DriveTrain.wheelSpeed[0]);
+            if (Robot.LineTracker.Sensors[0].get()) {
+                //If the left line sensor is tripped, then increase left wheel speed and decrese right wheel speed by a set proportion
+                DriveTrain.wheelSpeed[0] *= 1.08;
+                DriveTrain.wheelSpeed[1] *= 0.95; 
+            } else if  (Robot.LineTracker.Sensors[1].get()){
+                //If the right line sensor is tripped, then decrease the left wheel speed and increase the right wheel speed by a set proportion
+                DriveTrain.wheelSpeed[0] *= 0.95;
+                DriveTrain.wheelSpeed[1] *= 1.08; 
+            }
 
-        //Always run the right motor based on right motor input
-        DriveTrain.frontRightMotor.set(ControlMode.PercentOutput, DriveTrain.wheelSpeed[1]);
-/*
-        if(Robot.LineTracker.AUTO && Robot.LineTracker.rotateDirection != 0){
+        } else if(Robot.LineTracker.distanceToTravel <= 0) {
             //Rotate in place to get the robot lined up with the line
             DriveTrain.wheelSpeed[0] = Robot.LineTracker.rotateDirection;
             DriveTrain.wheelSpeed[1] = Robot.LineTracker.rotateDirection;
 
-        //} else if(Robot.LineTracker.distanceToTravel > 0) {
+        } else if(Robot.LineTracker.distanceToTravel > 0) {
             //Drive Forward
-            //DriveTrain.wheelSpeed [1] = -0.5;
-            //DriveTrain.wheelSpeed[0] = 0.5;
+            DriveTrain.wheelSpeed [1] = 1;
+            DriveTrain.wheelSpeed[0] = -1;
 
-        }*/
+        }
 
 
-        
+        DriveTrain.frontLeftMotor.set(ControlMode.PercentOutput, DriveTrain.wheelSpeed[0]);
+        DriveTrain.frontRightMotor.set(ControlMode.PercentOutput, DriveTrain.wheelSpeed[1]);
     }
     
 
--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/subsystems/Elevator.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/subsystems/Elevator.java
@@ -4,17 +4,16 @@
 package org.usfirst.frc.team451.robot.subsystems;
 
 import org.usfirst.frc.team451.robot.OI;
-//import org.usfirst.frc.team451.robot.commands.Drive;
+import org.usfirst.frc.team451.robot.commands.Drive;
 import org.usfirst.frc.team451.robot.commands.ElevatorMove;
 
 import com.ctre.phoenix.motorcontrol.can.*;
-//import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 
 public class Elevator extends Subsystem {
-	public static double elevatorHeightMargin = 0.5;//in inches
 
 	//heights for the center of the hatch panel spots on the rocket
 	static double firstHatchHeight = 12+7;//19 inches
@@ -33,43 +32,16 @@ public class Elevator extends Subsystem {
 	public static double WheelDiameter = 1.7284; //inches
 	public static double inchesPerCount = (Math.PI*WheelDiameter)/(countsPerRevolution*gearReduction); //the amount of inches that are covered in one count
 
-	public static WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(5);
+	// Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
+	public static WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(5);
+	
+	
+    
     public Elevator() {
     }
  
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
-	
-	/** Runs the user override commands from the x-box controller. PrintMethodData prints 
-	 * information to the console.
-	 */
-	public static void RunUserOverride(boolean PrintMethodData){
-		if (OI.mechBox.getY() > 0.000) {
-			elevatorMotor.set(-1);
-			if(PrintMethodData) System.out.println("Elevator UP (user)");
-		   } else if (OI.mechBox.getY() < 0.000) {
-			 elevatorMotor.set(1);
-			 if(PrintMethodData) System.out.println("Elevator DOWN (user)");
-		   }
-	}
-
-	/** Moves elevator based on TargetHeight (inches). PrintMethodData prints 
-	 * information to the console.
-	 */
-	public static void MoveTowards(double TargetHeight, boolean PrintMethodData){
-		if(elevatorMotor.getSelectedSensorPosition() > (TargetHeight+elevatorHeightMargin)/inchesPerCount){
-			elevatorMotor.set(1);
-			if(PrintMethodData) System.out.println("Elevator DOWN (preset)");
-		}
-
-		if(elevatorMotor.getSelectedSensorPosition() < (TargetHeight-elevatorHeightMargin)/inchesPerCount){
-			elevatorMotor.set(-1);
-			if(PrintMethodData) System.out.println("Elevator UP (preset)");
-		}
-	}
-
-
     
 	@Override
 	protected void initDefaultCommand() {
