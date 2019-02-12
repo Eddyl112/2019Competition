76f3113fbe5e30271c74de3b836dd5a87a9a164a
Alex Pererson <troop2fish@gmail.com>
2019-02-11 19:09:31 -0500

    Added and implemented elevator methods
    Added and implemented a method to move the robot to a set position in inches, and a method to override that when the user was moving the joystick on the x-bot controller.

--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/ElevatorMove.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/ElevatorMove.java
@@ -7,16 +7,17 @@
 
 package org.usfirst.frc.team451.robot.commands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.ControlMode;
 //import com.sun.java.swing.plaf.windows.TMSchema.Control;
 
 import org.usfirst.frc.team451.robot.OI;
 import org.usfirst.frc.team451.robot.Robot;
+import org.usfirst.frc.team451.robot.subsystems.Elevator;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ElevatorMove extends Command {
-  private static final String Elevator = null;
+  //private static final String Elevator = null;
 
 public ElevatorMove() {
     // Use requires() here to declare subsystem dependencies
@@ -31,13 +32,11 @@ public ElevatorMove() {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if (OI.mechBox.getY() > 0.000) {
-     Robot.Elevator.elevatorMotor.set(-1);
-     System.out.println("Elevator UP");
-    } else if (OI.mechBox.getY() < 0.000) {
-      Robot.Elevator.elevatorMotor.set(1);
-      System.out.println("Elevator DOWN");
-    }
+    //Go to the preset (run first so it does not override the user override)
+    Elevator.MoveTowards(0, false);
+
+    //Only run this method when the user is trying to override
+    if(Math.abs(OI.mechBox.getY()) > 0) Elevator.RunUserOverride(false);
   }
 
   // Make this return true when this Command no longer needs to run execute()
