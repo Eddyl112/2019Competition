2c6d75e7b8d556ed3aeed4ea3e52b144422a95f4
Eddyl112 <edwardlui17@gmail.com>
2019-03-05 21:29:55 -0500

    Final changes
    Added limity switch and tippy code

--- 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/ElevatorMove.java
+++ 2DTractionDifferentialArcadeDrive/src/main/java/org/usfirst/frc/team451/robot/commands/ElevatorMove.java
@@ -36,6 +36,11 @@ public ElevatorMove() {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+
+    if (Elevator.elevatorZero.get()) {
+      Elevator.elevatorMotor.setSelectedSensorPosition(0);
+    }
+
     //SET ELEVATOR TARGET POSITION
     //Hatch Presets
     if(OI.mechBox.getYButton()) {
@@ -51,38 +56,47 @@ public ElevatorMove() {
       Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
     }
 
-    //Port Presets
+    if (Elevator.ypr[1] > 20 || Elevator.ypr[1] < -20) {
+      Elevator.moveToPosition(0);
+    } else if (Elevator.ypr[2] > 20 || Elevator.ypr[2] < -20) {
+      Elevator.moveToPosition(0);
+    } else {
+      //Port Presets
       /*Preset elevator levels on dpad. This is the ONLY PLACE where any dpad code is; there is 
       NONE in OI because the XBOX dpad is very unique. 0 is UP, 90 is RIGHT, 180 is DOWN, and 270 
       is LEFT. You can also get intermediates at each 45 degree interval between those numbers, 
       we're not going to to leave some dead zones for the mech driver + it's not applicable here
       anyway. */
-    if (OI.mechBox.getPOV() == 0) {
-      Elevator.TargetHeightInInches = Elevator.PortHeights[2];
-      Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
-    } else if (OI.mechBox.getPOV() == 90 || OI.mechBox.getPOV() == 270) {
-      Elevator.TargetHeightInInches = Elevator.PortHeights[1];
-      Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
-    } else if (OI.mechBox.getPOV() == 180) {
-      Elevator.TargetHeightInInches = Elevator.PortHeights[0];
-      Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
+      if (OI.mechBox.getPOV() == 0) {
+        Elevator.TargetHeightInInches = Elevator.PortHeights[2];
+        Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
+      } else if (OI.mechBox.getPOV() == 90 || OI.mechBox.getPOV() == 270) {
+        Elevator.TargetHeightInInches = Elevator.PortHeights[1];
+        Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
+      } else if (OI.mechBox.getPOV() == 180) {
+        Elevator.TargetHeightInInches = Elevator.PortHeights[0];
+        Elevator.TargetHeightInTicks = (Elevator.TargetHeightInInches-Elevator.minHeight)/(Elevator.inchesPerCount);
+      }
+  
+      //RESETS ENCODER POSITION TO ZERO
+      // if (OI.mechBox.getRawButtonPressed(7)) {
+      //   Elevator.elevatorMotor.setSelectedSensorPosition(0);
+      // }
+  
+      //RUN ELEVATOR MOTION
+      if(OI.mechBox.getY(Hand.kRight) > 0.03){
+        //Control motors directly when user is overriding
+        Elevator.elevatorMotor.set(ControlMode.PercentOutput, OI.mechBox.getY(Hand.kRight));
+        //Set the target height in ticks equal to current height so it doesn't try to move back when you stop overriding
+        Elevator.TargetHeightInTicks = Elevator.elevatorMotor.getSelectedSensorPosition();
+      } else {
+        //uses motion magic to get to the proper height when user is not overriding
+        Elevator.moveToPosition(Elevator.TargetHeightInTicks);
+      }
     }
 
-    //RESETS ENCODER POSITION TO ZERO
-    if (OI.mechBox.getRawButtonPressed(7)) {
-      Elevator.elevatorMotor.setSelectedSensorPosition(0);
-    }
+    
 
-    //RUN ELEVATOR MOTION
-    if(OI.mechBox.getY(Hand.kRight) > 0.03){
-      //Control motors directly when user is overriding
-      Elevator.elevatorMotor.set(ControlMode.PercentOutput, OI.mechBox.getY(Hand.kRight));
-      //Set the target height in ticks equal to current height so it doesn't try to move back when you stop overriding
-      Elevator.TargetHeightInTicks = Elevator.elevatorMotor.getSelectedSensorPosition();
-    } else {
-      //uses motion magic to get to the proper height when user is not overriding
-      Elevator.moveToPosition(Elevator.TargetHeightInTicks);
-    }
 /*
     //Go to the preset (run first so it does not override the user override)
     //Elevator.MoveTowards(Elevator.TargetHeight, false);
